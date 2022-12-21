26c94e14f9352f5365c42d71088778de0d687a49
Alejandro Exposito <79151983+AlejandroDAM29@users.noreply.github.com>
2022-12-21 08:32:37 +0100

    Initial commit

---
+++ app/src/main/java/ae/mycourse/pruebahilt/MainActivity.kt
@@ -0,0 +1,11 @@
+package ae.mycourse.pruebahilt
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
